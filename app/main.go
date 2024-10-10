f84582b9a12d8ef212d2bfae60afeab3dcc75bba
Andrew Rodriguez <andrewrod003@gmail.com>
2024-10-10 16:13:15 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -13,7 +13,6 @@ var _ = exec.Command
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage!
 
@@ -21,11 +20,11 @@ func main() {
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
 	if err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
-
-	fmt.Println(string(output))
 }
