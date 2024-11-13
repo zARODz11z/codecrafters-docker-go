44c0f9a46d0b2a2b7fe98183e205e86af10a8741
Andrew Rodriguez <andrewrod003@gmail.com>
2024-11-12 16:11:41 -0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func createChroot() string {
@@ -29,6 +30,21 @@ func main() {
 		cmd := exec.Command("chroot", append([]string{dir}, os.Args[3:]...)...)
 		cmd.Dir = dir
 		cmd.Stdout = os.Stdout
+		// This is the key addition for PID namespacing
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Cloneflags: syscall.CLONE_NEWPID, // Create a new PID namespace
+		}
+
+		/*
+			cmd.SysProcAttr is a field of the exec.Cmd struct that allows you to
+			control the system call that's used to create the new process.
+
+			syscall.SysProcAttr is a struct that holds attributes that will be
+			applied to the new process.  One of these attributes is Cloneflags,
+			which allows you to specify flags to be passed to the clone() system call.
+			For basic process creation, fork() is sufficient. But when you need more control
+			over the relationship between processes, especially when dealing with containers and isolation, clone() is essential.
+		*/
 		cmd.Stderr = os.Stderr
 		if err := cmd.Run(); err != nil {
 			fmt.Printf("%v\n", err)
